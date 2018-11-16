c0fab51c9e7fe2bc5fe81d19a64e93a7d48f472b
LGBSPL\tnowak <tadeusz.nowak@euvic.pl>
2018-11-16 19:55:43 +0100

    Added movement system; Added simple AI following system

---
+++ Hackaton2019/Assets/Scripts/MovementController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour {
+
+    private Vector3 velocity;
+    private Rigidbody myRigidbody;
+
+    [SerializeField]
+    private Camera viewCamera;
+
+
+    public float moveSpeed = 5;
+
+    private void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void LookAt(Vector3 lookPoint)
+    {
+        Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(heightCorrectedPoint);
+    }
+
+    public void FixedUpdate()
+    {
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+
+    }
+
+    void Update()
+    {
+        Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 moveVelocity = moveInput.normalized * moveSpeed;
+        Move(moveVelocity);
+
+        Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            Vector3 point = ray.GetPoint(rayDistance);
+            Debug.DrawLine(ray.origin, point, Color.red);
+            //Debug.DrawRay(ray.origin,ray.direction * 100,Color.red);
+            LookAt(point);
+        }
+
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 10, Color.black);
+    }
+}
