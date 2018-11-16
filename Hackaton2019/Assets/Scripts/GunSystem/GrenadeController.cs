426c5854890545e67d1e93246dae38ab742fd048
Piotr Wesolowski <piotr.wesolowski@euvic.pl>
2018-11-17 00:24:09 +0100

    greande vol1

---
+++ Hackaton2019/Assets/Scripts/GunSystem/GrenadeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeController : MonoBehaviour
+{
+    public float grenadeAmount;
+    public GameObject grenadePrefab;
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            ThrowGrenade();
+        }
+    }
+
+    private void ThrowGrenade()
+    {
+        GameObject newGrenade = Instantiate(grenadePrefab, transform.position, Quaternion.identity);
+    }
+}
