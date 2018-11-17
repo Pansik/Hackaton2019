b2bbdb0723d3252ddd62db88d2d5ef3e2f41fd11
Piotr Wesolowski <piotr.wesolowski@euvic.pl>
2018-11-17 04:20:15 +0100

    Add enemy tag to enemies, make item granade rotate

---
+++ Hackaton2019/Assets/Scripts/RotateAnimation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAnimation : MonoBehaviour
+{
+    public float rotationSpeed;
+
+	void Update ()
+    {
+        transform.Rotate(transform.up, 1 * rotationSpeed * Time.deltaTime);
+	}
+}
