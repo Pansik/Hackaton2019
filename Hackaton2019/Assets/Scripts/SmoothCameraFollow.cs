edad0b213b1cfc69686a4fa1dd2da1ae2247ff58
LGBSPL\tnowak <tadeusz.nowak@euvic.pl>
2018-11-17 13:51:33 +0100

    Working on camera

---
+++ Hackaton2019/Assets/Scripts/SmoothCameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+
+    public float smoothSpeed = 0.125f;
+    public Vector3 offset;
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        //var temp = new Vector3(smoothedPosition.x, transform.position.y, smoothedPosition.z);
+        transform.position = smoothedPosition;
+        
+    }
+}
